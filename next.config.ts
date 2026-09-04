85f0062a2e6b9ff63d3fbd72a42617aef2923a4b
GulamMd <gmkhalid11@gmail.com>
2026-09-04 22:39:45 +0530

    Fixed some broken changes

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
-import type { NextConfig } from "next";
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
-const nextConfig: NextConfig = {};
-
-export default nextConfig;
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: env("DIRECT_URL"),
+  },
+});
